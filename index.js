2a75c07e1cb1aa39bbb2d411e1e5b70464db6c0c
Eric <eric2620371@gmail.com>
2025-11-29 13:43:37 +0000

    hero page done

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ef65cc72cbeeb3a07594217b313876732c3f7e3476c67af93373b0a4034f82b
-size 576
+oid sha256:70710ee2cde6e519c212f3ce43fb2d7af2853426c681ed27c38f147cd752d61a
+size 580
---
+++ test.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2f1e749da8784c87063da3643cb250df4586d853e9b79febfff3c53d2d9725a2
+size 482
