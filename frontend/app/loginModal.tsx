74d8836dc33d80e157ffb691cce989f1028f07c5
Eric Golovatenko Simões <eric2620371@gmail.com>
2026-01-17 15:42:00 +0000

    added cafe photo

--- frontend/app/loginModal.tsx
+++ frontend/app/loginModal.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import Button from "@/component/button";
 import Image from "next/image";
 
-interface LoginModalProps{
+interface LoginModalProps {
     onClose: () => void;
 }
 
 const LoginModal = ({onClose}: LoginModalProps) => {
     return (
         <div className="z-2 fixed top-0 left-0 w-screen h-screen bg-black/70 flex justify-center items-center">
-            <div className="bg-gray-600 rounded-md w-8/12 h-8/12">
-                <div className="w-full flex justify-end p-1">
+            <div className="bg-gray-600 rounded-md w-8/12 h-8/12 relative flex overflow-hidden">
                     <Button type="secondary" content={
-                      <Image src="/svgs/x.svg" width="30" height="30" alt="Close Login Modal Button" className="transition duration-200 ease-in-out hover:invert-30 cursor-pointer"/>
+                        <Image src="/svgs/x.svg" width="30" height="30" alt="Close Login Modal Button"
+                               className="transition duration-200 ease-in-out hover:invert-30 cursor-pointer absolute top-4 right-4"/>
                     } doOnClick={onClose}/>
-
+                <div className="w-1/3 h-full bg-neutral-100 p-0 top-0 left-0 ">
+                    <Image src="/images/japanese cafe.png" alt="Some image" width="500" height="500" className="object-cover"></Image>
                 </div>
             </div>
         </div>
--- frontend/component/button.tsx
+++ frontend/component/button.tsx
@@ -9,15 +9,15 @@ interface ButtonProps {
 }
 
 
-const Button = ({content, type ="main", doOnClick} : ButtonProps) => {
-    
+const Button = ({content, type = "main", doOnClick}: ButtonProps) => {
+
     return (
         <div>
             <button className={clsx("rounded-xl m-0 transition duration-200 ease-in-out antialiased cursor-pointer", {
-                "px-3 py-2 bg-accent hover:bg-accentDark" : type === "main",
-                "px-3 py-2 hover:text-neutral-400" : type === "secondary"
+                "px-3 py-2 bg-accent hover:bg-accentDark": type === "main",
+                "px-3 py-2 hover:text-neutral-400": type === "secondary"
             })}
-            onClick={doOnClick}>
+                    onClick={doOnClick}>
                 {content}
             </button>
         </div>
