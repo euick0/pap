f2e1093f80ca5f9a96acf708f55194a5dd944b3b
Eric Golovatenko Simões <118106151+euick0@users.noreply.github.com>
2026-01-18 19:24:35 +0000

    small fix

--- frontend/app/heroText.tsx
+++ frontend/app/heroText.tsx
@@ -4,7 +4,7 @@ const HeroText = () => {
     return (
         <div className="w-screen h-screen flex flex-col items-center justify-center">
             <h1 className="text-center text-6xl text font-semibold ">Learn any language</h1>
-            <h1 className="text-center text-6xl text font-bold p-1 bg-linear-to-tr from-primary to-complement1 bg-clip-text text-transparent">Travel wherever you want</h1>
+            <h1 className="text-center text-6xl text font-bold p-1 bg-linear-to-tr from-primary to-complement bg-clip-text text-transparent">Travel wherever you want</h1>
         </div>
     );
 };
